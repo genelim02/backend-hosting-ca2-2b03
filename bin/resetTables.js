202ba4f991dababa5c87e858ce3348767a73d521
genelim02 <genelim02@yahoo.com>
2020-12-09 03:10:10 +0800

    Made changes

--- bin/resetTables.js
+++
@@ -1,9 +0,0 @@
-const { resetTables, closeDatabaseConnections } = require('../database');
-const chalk = require('chalk');
-
-resetTables()
-    .then(function (res) {
-        console.log(chalk.green('Success!'));
-    })
-    .catch(console.error)
-    .finally(closeDatabaseConnections);
